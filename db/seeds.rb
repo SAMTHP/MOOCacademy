e1a445487b8122dee43b259c0a8e5e70420968bf
Vano <hervelee99@gmail.com>
2018-10-24 19:53:59 +0200

    ajout seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,12 @@
-require 'faker'
+
 # 10.times do
 #   classes = Cour.create!(titre: Faker::Company.name, description: Faker::Company.name)
 # end
+require 'faker'
+10.times do
+  cour = Cour.create!(titre: Faker::Company.name,description: Faker::Company.name)
+end
 
-100.times do
+10.times do
   lessons = Lesson.create!(cour_id: rand(1..10), titre: Faker::Company.name, body: Faker::Company.name)
 end
